f3094b55c0b623a6da6458427f847c8678b7c97b
Fernando Filho <fernando.fs.filhorn@gmail.com>
2022-07-05 13:58:30 -0300

    Código finalizado.
    Pequeno ajuste.

--- imparVSpares.js
+++ imparVSpares.js
@@ -2,7 +2,7 @@ const tabelaValores = [1, 3, 8, 6, 9, 4, 16, 21]
 var tabelaPares = []
 var tabelaImpares = []
 for (c = 0; c <= (tabelaValores.length-1); c++){
-    if      (tabelaValores[c]%2 !=0){tabelaPares.push(tabelaValores[c])}
+    if      (tabelaValores[c]%2 ==0){tabelaPares.push(tabelaValores[c])}
     else    {tabelaImpares.push(tabelaValores[c])}
 }
 console.log(tabelaImpares)
