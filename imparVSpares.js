35a27f416b781f9c2c614aedb2d39d05a96853be
Fernando-Filho <fernando.fs.filhorn@gmail.com>
2022-07-04 23:07:56 -0300

    Finalizado o separadaor de valores impares e pares

--- imparVSpares.js
+++ imparVSpares.js
@@ -1,16 +1,10 @@
 const tabelaValores = [1, 3, 8, 6, 9, 4, 16, 21]
 var tabelaPares = []
 var tabelaImpares = []
-var c = 0
-
-do{
-    if(tabelaValores[c]%2 !== 0) {
-        tabelaImpares.push(tabelaValores[c])
-    }
-    else{
-        tabelaPares.push(tabelaValores[c])
-    }
-    c = c++
-}while (c <= tabelaValores.length)
-console.log(tabelaValores[c])
-
+for (c = 0; c <= (tabelaValores.length-1); c++){
+    if      (tabelaValores[c]%2 !=0){tabelaPares.push(tabelaValores[c])}
+    else    {tabelaImpares.push(tabelaValores[c])}
+}
+console.log(tabelaImpares)
+console.log(tabelaPares)
+console.log(c)
